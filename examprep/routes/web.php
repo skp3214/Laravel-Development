4e5048cae9ca6feca30161cf99fff29281c55b0d
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-27 22:03:23 +0530

    added user registration routes with controller methods for displaying the form and handling submissions

--- examprep/routes/web.php
+++ examprep/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FileUploadController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,7 @@ Route::get('/', function () {
 
 Route::get('/upload', [FileUploadController::class, 'showUploadForm'])->name('upload.form');
 Route::post('/upload', [FileUploadController::class, 'handleFileUpload'])->name('upload.file');
+
+
+Route::get('/register', [UserController::class, 'showRegisterForm'])->name('register.form');
+Route::post('/register', [UserController::class, 'handleRegistration'])->name('register.submit');
