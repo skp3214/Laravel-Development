aef5301a2b3b8fb68f1a943aebaf176162e6479b
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-27 22:38:56 +0530

    refactored authentication routes to use AuthController and updated registration routes

--- examprep/routes/web.php
+++ examprep/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FileUploadController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,5 +14,13 @@ Route::get('/upload', [FileUploadController::class, 'showUploadForm'])->name('up
 Route::post('/upload', [FileUploadController::class, 'handleFileUpload'])->name('upload.file');
 
 
-Route::get('/register', [UserController::class, 'showRegisterForm'])->name('register.form');
-Route::post('/register', [UserController::class, 'handleRegistration'])->name('register.submit');
+// Route::get('/register', [UserController::class, 'showRegisterForm'])->name('register.form');
+// Route::post('/register', [UserController::class, 'handleRegistration'])->name('register.submit');
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+Route::get('/profile', [AuthController::class, 'profile'])->middleware('auth')->name('profile');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
